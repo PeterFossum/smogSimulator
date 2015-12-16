a4ba3ed16f18c3fd942bedd70ea0e21984b859d5
Peter Fossum <pgfossum@gmail.com>
2015-12-15 17:27:14 -0800

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,16 +4,19 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home - SmogCheckNV Emissions Academy"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About - SmogCheckNV Emissions Academy"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
+    assert_select "title", "Contact - SmogCheckNV Emissions Academy"
   end
 
 end
